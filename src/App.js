434cfeccd986100cce4d9b4b9a40a85211985ad7
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    added new project text into the App Component of the React application.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Tyler and this is my project </p>
+        <p>
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
